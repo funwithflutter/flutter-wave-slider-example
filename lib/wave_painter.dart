413ce473aebe02fa58c58c833a53108840b4e256
Gordon <funwithflutter@gmail.com>
2018-12-09 16:34:39 +0800

    Part 4: Bend It

--- lib/wave_painter.dart
+++ lib/wave_painter.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WavePainter extends CustomPainter {
@@ -8,6 +10,9 @@ class WavePainter extends CustomPainter {
 
   final Paint wavePainter;
   final Paint fillPainter;
+
+  /// Previous slider position initialised at 0.0
+  double _previousSliderPosition = 0.0;
 
   WavePainter(
       {@required this.sliderPosition,
@@ -40,7 +45,31 @@ class WavePainter extends CustomPainter {
   }
 
   _paintWaveLine(Canvas canvas, Size size) {
-    // Wave-line definitions.
+    WaveCurveDefinitions waveCurve = _calculateWaveLineDefinitions();
+
+    Path path = Path();
+    path.moveTo(0.0, size.height);
+    path.lineTo(waveCurve.startOfBezier, size.height);
+    path.cubicTo(
+        waveCurve.leftControlPoint1,
+        size.height,
+        waveCurve.leftControlPoint2,
+        waveCurve.controlHeight,
+        waveCurve.centerPoint,
+        waveCurve.controlHeight);
+    path.cubicTo(
+        waveCurve.rightControlPoint1,
+        waveCurve.controlHeight,
+        waveCurve.rightControlPoint2,
+        size.height,
+        waveCurve.endOfBezier,
+        size.height);
+    path.lineTo(size.width, size.height);
+
+    canvas.drawPath(path, wavePainter);
+  }
+
+  WaveCurveDefinitions _calculateWaveLineDefinitions() {
     double bendWidth = 40.0;
     double bezierWidth = 40.0;
 
@@ -57,39 +86,75 @@ class WavePainter extends CustomPainter {
     double rightControlPoint1 = endOfBend;
     double rightControlPoint2 = endOfBend;
 
-    double bendValue = 0.0;
-    bool moveLeft = false;
-
-    if (moveLeft) {
-      leftControlPoint1 = leftControlPoint1 + bendValue;
-      leftControlPoint2 = leftControlPoint2 + bendValue / 2;
-      rightControlPoint1 = rightControlPoint1 + bendValue;
-      rightControlPoint2 = rightControlPoint2 - bendValue;
-
-      centerPoint = centerPoint + bendValue;
-    } else {
-      leftControlPoint1 = leftControlPoint1 + bendValue;
-      leftControlPoint2 = leftControlPoint2 - bendValue;
-      rightControlPoint1 = rightControlPoint1 - bendValue / 2;
-      rightControlPoint2 = rightControlPoint2 - bendValue;
-
-      centerPoint = centerPoint - bendValue;
+    double bendability = 25.0;
+    double maxSlideDifference = 20.0;
+    double slideDifference = (sliderPosition - _previousSliderPosition).abs();
+    if (slideDifference > maxSlideDifference) {
+      slideDifference = maxSlideDifference;
     }
 
-    Path path = Path();
-    path.moveTo(0.0, size.height);
-    path.lineTo(startOfBezier, size.height);
-    path.cubicTo(leftControlPoint1, size.height, leftControlPoint2,
-        controlHeight, centerPoint, controlHeight);
-    path.cubicTo(rightControlPoint1, controlHeight, rightControlPoint2,
-        size.height, endOfBezier, size.height);
-    path.lineTo(size.width, size.height);
+    double bend =
+        lerpDouble(0.0, bendability, slideDifference / maxSlideDifference);
 
-    canvas.drawPath(path, wavePainter);
+    bool moveLeft = sliderPosition < _previousSliderPosition;
+    if (moveLeft) {
+      leftControlPoint1 = leftControlPoint1 - bend;
+      leftControlPoint2 = leftControlPoint2 + bend;
+      rightControlPoint1 = rightControlPoint1 + bend;
+      rightControlPoint2 = rightControlPoint2 - bend;
+      centerPoint = centerPoint + bend;
+    } else {
+      leftControlPoint1 = leftControlPoint1 + bend;
+      leftControlPoint2 = leftControlPoint2 - bend;
+      rightControlPoint1 = rightControlPoint1 - bend;
+      rightControlPoint2 = rightControlPoint2 + bend;
+
+      centerPoint = centerPoint - bend;
+    }
+
+    WaveCurveDefinitions waveCurveDefinitions = WaveCurveDefinitions(
+      controlHeight: controlHeight,
+      startOfBezier: startOfBezier,
+      endOfBezier: endOfBezier,
+      leftControlPoint1: leftControlPoint1,
+      leftControlPoint2: leftControlPoint2,
+      rightControlPoint1: rightControlPoint1,
+      rightControlPoint2: rightControlPoint2,
+      centerPoint: centerPoint,
+    );
+
+    return waveCurveDefinitions;
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
+  bool shouldRepaint(WavePainter oldDelegate) {
+    this._previousSliderPosition = oldDelegate.sliderPosition;
+    if (sliderPosition != oldDelegate.sliderPosition) {
+      return true;
+    } else {
+      return false;
+    }
   }
+}
+
+class WaveCurveDefinitions {
+  final double startOfBezier;
+  final double endOfBezier;
+  final double leftControlPoint1;
+  final double leftControlPoint2;
+  final double rightControlPoint1;
+  final double rightControlPoint2;
+  final double controlHeight;
+  final double centerPoint;
+
+  WaveCurveDefinitions({
+    @required this.startOfBezier,
+    @required this.endOfBezier,
+    @required this.leftControlPoint1,
+    @required this.leftControlPoint2,
+    @required this.rightControlPoint1,
+    @required this.rightControlPoint2,
+    @required this.controlHeight,
+    @required this.centerPoint,
+  });
 }
